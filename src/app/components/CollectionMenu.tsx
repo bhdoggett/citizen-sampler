5ab1d0f2dbc46dd3bcb77777f237a548fe7dd96b
Ben Doggett <bdoggett@gmail.com>
2025-04-11 15:14:37 -0400

    Style Collection Menu.

--- src/app/components/CollectionMenu.tsx
+++ src/app/components/CollectionMenu.tsx
@@ -9,12 +9,15 @@ const CollectionMenu = () => {
   };
 
   return (
-    <div>
-      <label htmlFor="collection">Select a Collection: </label>
+    <div className="border border-black rounded-md bg-slate-800  m-3 p-1 shadow-md shadow-slate-800">
+      <label htmlFor="collection" className="text-white">
+        Choose a Collection:{" "}
+      </label>
       <select
         name="collection"
         id="collection"
         onChange={(e) => handleSelect(e.target.value)}
+        className="shadow-inner shadow-slate-700 shadoow-"
       >
         {collectionNames.map((collection) => (
           <option key={collection} value={collection}>
