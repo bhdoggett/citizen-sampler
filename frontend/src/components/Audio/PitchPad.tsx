8724822283625d76acb3489b133e48c48cdb243c
Ben Doggett <bdoggett@gmail.com>
2025-05-29 23:58:13 -0400

    Remove useEffect from PitchPad

--- frontend/src/components/Audio/PitchPad.tsx
+++ frontend/src/components/Audio/PitchPad.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import * as Tone from "tone";
 import { useAudioContext } from "../../app/contexts/AudioContext";
 import { CustomSampler } from "../../types/CustomSampler";
