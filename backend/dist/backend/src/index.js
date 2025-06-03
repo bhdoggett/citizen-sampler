0020be4ca15e3f02f29a3c0a597467bb52772086
Ben Doggett <bdoggett@gmail.com>
2025-06-03 19:06:03 -0400

    WIP CORS issues

--- backend/dist/backend/src/index.js
+++ backend/dist/backend/src/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const passport_1 = __importDefault(require("passport"));
@@ -14,39 +16,39 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const isDevelopment = process.env.NODE_ENV === "development";
-// app.use(cors());
-if (isDevelopment) {
-    app.use((0, cors_1.default)({
-        origin: "http://localhost:3000",
-        credentials: true,
-    }));
-}
-else {
-    app.use((0, cors_1.default)({
-        origin: "https://www.citizensampler.com",
-        credentials: true,
-    }));
-}
+app.use(cors());
+// if (isDevelopment) {
+//     app.use((0, cors_1.default)({
+//         origin: "http://localhost:3000",
+//         credentials: true,
+//     }));
+// }
+// else {
+//     app.use((0, cors_1.default)({
+//         origin: "https://www.citizensampler.com",
+//         credentials: true,
+//     }));
+// }
 const PORT = process.env.PORT || 8000;
 app.use(passport_1.default.initialize());
 app.use(express_1.default.json());
 app.use("/auth", auth_1.default);
 app.use("/beats", beats_1.default);
 if (!keys_1.default.MONGO_URI) {
-    throw new Error("Missing MONGO_URI in environment variables");
+  throw new Error("Missing MONGO_URI in environment variables");
 }
 mongoose_1.default.connect(keys_1.default.MONGO_URI);
 const db = mongoose_1.default.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Connected to MongoDB");
+  console.log("Connected to MongoDB");
 });
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 app.get("/test", (req, res) => {
-    res.send("Hello Test!");
+  res.send("Hello Test!");
 });
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
